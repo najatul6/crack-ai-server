daa7908b63aa67976337586a3584c42a17bd1085
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-03 23:14:04 +0600

    update data and status

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) =>{
-    res.send("Hello, this is the API endpoint for the simple Express server")
+    res.send({data:"Hello, this is the API endpoint for the simple Express server",status: 200})
 })
 app.listen(port,()=>{
     console.log("server listening on port"+port);
