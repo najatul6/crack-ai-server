de9b438c8e7d56fc37a767b1b02cece3573586d4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-03 23:37:54 +0600

    add some code for gemini

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+// Make sure to include these imports:
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Write a story about a magic backpack.";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
+
 app.get('/', (req, res) =>{
     res.send({data:"Hello, this is the API endpoint for the simple Express server",status: 200})
 })
