4c1edef21ef4450e22e101bb0f7015e68c46348b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-03 23:11:39 +0600

    get response from server

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+
+app.get('/', (req, res) =>{
+    res.send("Hello, this is the API endpoint for the simple Express server")
+})
+app.listen(port,()=>{
+    console.log("server listening on port"+port);
+})
