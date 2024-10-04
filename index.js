836bbbb38b898e3306762ec3c8fdc469a50191a7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-04 20:01:09 +0600

    crate prompt = req.body. and add an text area

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const express = require('express')
-const app = express()
-require ("dotenv").config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-app.use(express.urlencoded({extends:true}))
+app.use(express.urlencoded({ extends: true }));
 
+const form = `
+<form method="POST" action="/prompt">
+<textarea name="prompt" id="prompt"></textarea>
+<button type="submit">Prompt</button>
+</form>
 
-app.post('/prompt',async(req, res)=>{
-    const prompt = "Write a story about a magic backpack.";
-    const result = await model.generateContent(prompt);
-    const text=result.response.text()
-    res.send({data: text,status:200});
-})
+`;
 
+app.get("/prompt", async(req, res) => {
+  res.send(form);
+});
 
+app.post("/prompt", async (req, res) => {
+  const prompt = req.body;
+  const result = await model.generateContent(prompt);
+  const text = result.response.text();
+  res.send({ data: text, status: 200 });
+});
 
-app.get('/', (req, res) =>{
-    res.send({data:"Hello, this is the API endpoint for the simple Express server",status: 200})
-})
-app.listen(port,()=>{
-    console.log("server listening on port"+port);
-})
+app.get("/", (req, res) => {
+  res.send({
+    data: "Hello, this is the API endpoint for the simple Express server",
+    status: 200,
+  });
+});
+app.listen(port, () => {
+  console.log("server listening on port" + port);
+});
