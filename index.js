a56d7591c29276716b86bbe8c9609fa08acb60d7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-04 19:50:07 +0600

    create post url

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express()
 require ("dotenv").config()
 const port = process.env.PORT || 5000
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-app.get('/prompt',async(req, res)=>{
+app.use(express.urlencoded({extends:true}))
+
+
+app.post('/prompt',async(req, res)=>{
     const prompt = "Write a story about a magic backpack.";
     const result = await model.generateContent(prompt);
     const text=result.response.text()
     res.send({data: text,status:200});
-
 })
 
 
