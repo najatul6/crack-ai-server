f427d589b5fed3fab5acd258a6bfa30efce8b48d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-06 23:11:41 +0600

    update db

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors= require("cors");
+const { connect } = require("./utils/dbConnect");
 
 // Global Middleware 
 app.use(cors())
 app.use(express.json())
 
 // Database Connect 
-connect
+connect();
 
 app.get("/", (req, res) => {
   res.send({
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `process.env.DATABASE_URL`;
+const uri = process.env.DATABASE_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
