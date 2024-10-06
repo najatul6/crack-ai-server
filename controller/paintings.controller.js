9d42c8055fa964bee03b026ac1b6c7938766db45
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-07 02:26:59 +0600

    generate paint worked now

--- controller/paintings.controller.js
+++ controller/paintings.controller.js
@@ -1,4 +1,10 @@
 const testRoute=(req,res)=>{
     res.send("Hello World form controller")
 }
+
+const generatePaint=async(req, res)=>{
+    const {body}=req||{}
+    const {prompt}=body||{}
+}
+
 module.exports={testRoute}
---
+++ utils/getImageData.js
@@ -0,0 +1,15 @@
+const getImageData=(propmt)=>{
+    const form = new FormData();
+    form.append("prompt", propmt);
+    fetch("https://clipdrop-api.co/text-to-image/v1", {
+        method: "POST",
+        headers: {
+          "x-api-key": process.env.CLIP_DROP_API,
+        },
+        body: form,
+      })
+        .then((response) => response.arrayBuffer())
+        .then((buffer)=>{
+            return buffer
+        })
+}
