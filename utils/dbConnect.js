7bd8f17cbcf179c5fb63b7d488ff625dd285ab0d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-06 23:25:48 +0600

    update and change database settings

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.DATABASE_URL;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DATABASE_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -8,22 +8,18 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
-let db;
-const connect = async () => {
+let db
+async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    db = client.db("crackAi-db");
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
-};
-module.exports = { db, connect };
+}
