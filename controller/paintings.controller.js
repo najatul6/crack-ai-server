22301493cf43ed110e11e4133a537c5a90707266
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-08 18:14:03 +0600

    create function for get all paintings

--- controller/paintings.controller.js
+++ controller/paintings.controller.js
@@ -3,15 +3,20 @@ const { db } = require("../utils/dbConnect");
 const { getImageData } = require("../utils/getImageData");
 const getImageUrl = require("../utils/getImageUrl");
 
-const paintingCollections = db.collection("paintings")
+const paintingCollections = db.collection("paintings");
 const testRoute = (req, res) => {
   res.send("Hello World form controller");
+};
+
+const getAllPaintings = (req, res) => {
+  const results = paintingCollections.find().toArray();
+  res.send(results);
 };
 
 const generatePaint = async (req, res) => {
   const { body } = req || {};
   const { prompt, email, category, type } = body || {};
-  const finalPrompt=`generate a beautiful ${type} of ${category} painting about ${prompt}`
+  const finalPrompt = `generate a beautiful ${type} of ${category} painting about ${prompt}`;
   const buffer = await getImageData(finalPrompt);
   const imageData = await getImageUrl(buffer, prompt);
   const data = {
@@ -19,23 +24,22 @@ const generatePaint = async (req, res) => {
     title: imageData?.data?.title,
     thumb: imageData?.data?.thumb?.url,
     imageUrl: imageData?.data?.url,
-    medium_url:imageData?.data?.medium?.url,
+    medium_url: imageData?.data?.medium?.url,
     email,
     category,
     type,
-    details:prompt,
-    date:new Date()
+    details: prompt,
+    date: new Date(),
   };
-  const result=await paintingCollections.insertOne(data)
+  const result = await paintingCollections.insertOne(data);
   res.send(result);
 };
 
-const singleImageDetails=async(req,res)=>{
+const singleImageDetails = async (req, res) => {
   const { id } = req.params;
-  const query= {_id: new ObjectId(id) }
+  const query = { _id: new ObjectId(id) };
   const painting = await paintingCollections.findOne(query);
   res.send(painting);
-  
-}
+};
 
-module.exports = { testRoute, generatePaint,singleImageDetails };
+module.exports = { testRoute, generatePaint, singleImageDetails,getAllPaintings };
